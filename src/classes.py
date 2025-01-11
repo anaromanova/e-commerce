b6f75816b29f89c7df75574f6509cef6e6af6193
AnastasiaR <anastasia.romanova3169@gmail.com>
2025-01-11 18:36:43 +0300

    coverage

--- src/classes.py
+++ src/classes.py
@@ -99,6 +99,7 @@ class Category:
 
 
 class Smartphone(Product):
+
     def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
         super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
